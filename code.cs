75b82acf2f7cb6450e66b890b7888e1e9c7bd68a
Nicolas Gieringer <webdevng@gmail.com>
2019-08-29 08:56:22 +0200

    Update code.cs

--- code.cs
+++ code.cs
@@ -1,1 +1,31 @@
 
+//In PluginConfiguration unter this.ReadAllowedAttachments(reader) im Konstructor
+
+this.ReadSupportedContainer(reader)
+
+// Neue Funtion unter die Funktion ReadAllowedAttachments
+
+internal void ReadSupportedContainer(IConfigurationReader reader)
+{
+  // TODO: richtig auslesen
+  // <Compressed>Wert<Compressed>
+  string compressed = reader.GetStringValue("SupportedCntainer.Compressed")
+  string mime = reader.GetStringValue("SupportedCntainer.Mime")
+  
+  string[] compressed_items = compressed.Split(' ');  
+  List<string> compressed_list = new List<string>(compressed_items);
+  
+  string[] mime_items = mime.Split(' ');  
+  List<string> mime_list = new List<string>(mime_items);
+ 
+  this.SupportedContainerCompressed = compressed_list;
+  this.SupportedContainerMime = mime_list;
+  
+}
+
+// In IPluginConfiguration unter List<IAttachments> AllowedAttachments 
+
+List<string> SupportedContainerCompressed {}
+List<string> SupportedContainerMime {}
+
+
